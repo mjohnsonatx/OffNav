6ddd7859f2764b229a1c23d9ef540d8de5830b3a
mjohnsonatx <77946919+mjohnsonatx@users.noreply.github.com>
2026-07-29 19:14:05 -0500

    initial commit

--- app/src/main/java/com/example/offnav/location/LocationController.kt
+++ app/src/main/java/com/example/offnav/location/LocationController.kt
@@ -37,6 +37,7 @@ class LocationController {
         }
     }
 
+
     fun lastLocation(map: MapLibreMap) =
         map.locationComponent.takeIf { it.isLocationComponentActivated }?.lastKnownLocation
 }
