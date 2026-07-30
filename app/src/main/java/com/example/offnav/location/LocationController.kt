dc2ee1ccb5c54703c2577952a1e5d67781d87f38
mjohnsonatx <77946919+mjohnsonatx@users.noreply.github.com>
2026-07-30 16:55:22 -0500

    implemented some performance solutions

--- app/src/main/java/com/example/offnav/location/LocationController.kt
+++ app/src/main/java/com/example/offnav/location/LocationController.kt
@@ -11,37 +11,6 @@ import org.maplibre.android.location.modes.RenderMode
 import org.maplibre.android.maps.MapLibreMap
 import org.maplibre.android.maps.Style
 
-//class LocationController {
-//
-//    @SuppressLint("MissingPermission") // caller guarantees permission
-//    fun enable(context: Context, map: MapLibreMap, style: Style, followUser: Boolean) {
-//        val options = LocationComponentOptions.builder(context)
-//            .pulseEnabled(true)
-//            .build()
-//
-//        val request = LocationEngineRequest.Builder(1000L)
-//            .setPriority(LocationEngineRequest.PRIORITY_HIGH_ACCURACY)
-//            .setFastestInterval(500L)
-//            .build()
-//
-//        map.locationComponent.apply {
-//            activateLocationComponent(
-//                LocationComponentActivationOptions.builder(context, style)
-//                    .locationComponentOptions(options)
-//                    .locationEngineRequest(request)
-//                    .useDefaultLocationEngine(true)
-//                    .build()
-//            )
-//            isLocationComponentEnabled = true
-//            renderMode = RenderMode.COMPASS
-//            cameraMode = if (followUser) CameraMode.TRACKING else CameraMode.NONE
-//        }
-//    }
-//
-//
-//
-//}
-
 class LocationController {
 
     @SuppressLint("MissingPermission")
@@ -68,8 +37,5 @@ class LocationController {
         map.locationComponent.takeIf { it.isLocationComponentActivated }
             ?.forceLocationUpdate(location)
     }
-
-    fun lastLocation(map: MapLibreMap) =
-        map.locationComponent.takeIf { it.isLocationComponentActivated }?.lastKnownLocation
 }
 
